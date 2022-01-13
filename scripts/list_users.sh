35ef37a14f1ab5ceed9bfa966b44032c8268b495
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2022-01-13 15:52:35 +0530

    Update list_users.sh

--- scripts/list_users.sh
+++ scripts/list_users.sh
@@ -28,17 +28,6 @@ user_info=`awk -F':' -v "min=${uid_min##UID_MIN}" -v "max=${uid_max##UID_MAX}" '
 
 echo $user_info
 
-# change password expiry info for the user(s)
-# good ref: https://www.thegeekstuff.com/2009/04/chage-linux-password-expiration-and-aging/
-# disable password aging
-  #chage -m 0 -M 99999 -I -1 -E -1 <user>
-  #sudo /usr/bin/chage -E "2025-12-01" -M 30000 -W 30 -I 10 $u 
-for u in $user_info
-do
-    echo $u
-    sudo /usr/bin/chage -M 5000 $u
-    chage -l $u
-done
 
 
  
