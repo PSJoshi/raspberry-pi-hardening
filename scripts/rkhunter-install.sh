b62e9dac50f0667aa7001e9bde51432b266ffe8c
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2022-09-01 11:31:42 +0530

    Update rkhunter-install.sh

--- scripts/rkhunter-install.sh
+++ scripts/rkhunter-install.sh
@@ -17,3 +17,10 @@ rkhunter --check --sk --rwo
 # All recent versions of rkhunter have a cronjob preinstalled under the /etc/cron.daily directory.
 # As a system admin, monitor rkhunter.log file and if any suspicious file change(s) are found, raise 
 #alerts
+
+###
+# Ref: 
+# https://www.woktron.com/secure/knowledgebase/79/Installation-Rootkit-Hunter-rkhunter-on-CentOS.html
+# https://isaac.tips/security/installing-rkhunter-on-ubuntu/
+# https://raw.githubusercontent.com/MediaCluster/SysAdminScripts/master/rkhunter-whitelist.sh
+
