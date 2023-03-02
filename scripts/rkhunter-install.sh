c76508d09eb100dfe30c04ccc3b9b9a53ac99a70
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2023-03-02 14:40:24 +0530

    Update rkhunter-install.sh

--- scripts/rkhunter-install.sh
+++ scripts/rkhunter-install.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 # Kindly make sure that EPEL repo is enabled in yum configuration.
 yum install rkhunter
+# Before scanning, do rkhunter configuration
+cat >> /etc/rkhunter.conf << EOF
+UPDATE_MIRRORS=0
+# The MIRRORS_MODE option tells rkhunter which mirrors are to be used when
+# the '--update' or '--versioncheck' command-line options are given.
+# Possible values are:
+#     0 - use any mirror
+#     1 - only use local mirrors
+#     2 - only use remote mirrors
 
-# Modify rkhunter configuration
-# UPDATE_MIRRORS=1
-sed -i -e 's/.*UPDATE_MIRROR.*/UPDATE_MIRROR=1/g' /etc/rkhunter.conf
-# MIRRORS_MODE=0
-sed -i -e 's/.*MIRRORS_MODE.*/MIRRORS_MODE=0/g' /etc/rkhunter.conf
-
+MIRRORS_MODE=0
+WEB_CMD=""
 MAIL-ON-WARNING=root@localhost
-#sed -i -e 's/^(\#?)(MAIL-ON-WARNING)(\=.*)/MAIL-ON-WARNING=root@localhost/g' /etc/rkhunter.conf
-sed -i -e 's/^.*MAIL-ON-WARNING.*)/MAIL-ON-WARNING=root@localhost/g' /etc/rkhunter.conf
-
 MAIL_CMD=mail -s "[rkhunter] Warnings found for ${HOST_NAME}"
-sed -i -e 's/.*MAIL-CMD.*/MAIL_CMD=mail -s "[rkhunter] Warnings found for ${HOST_NAME}"/g' /etc/rkhunter.conf
+EOF
+
+# modify default configuration
+cat >> /etc/default/rkhunter <<EOF
+CRON_DAILY_RUN="true"
+CRON_DB_UPDATE="false"
+DB_UPDATE_EMAIL="false"
+REPORT_EMAIL="root"
+APT_AUTOGEN="false"
+EOF
+
+# check if config is right
+sudo rkhunter -C
+
+# you can see the log by cat /var/log/rkhunter.log
+# or you can print warning to console only
 
 #updates rkhunter text data files
 rkhunter --update
@@ -22,7 +39,7 @@ rkhunter --update
 rkhunter --propupd
 
 #checks for rootkits without keypress needed & shows only warnings
-rkhunter --check --sk --rwo
+rkhunter --check --sk --rwo --enable all --disable none
 
 # RKHunter log file is: /var/log/rkhunter.log
 # You may get warning on some files/directories. To whitelist, ALLOWHIDDENDIR
@@ -30,6 +47,9 @@ rkhunter --check --sk --rwo
 # All recent versions of rkhunter have a cronjob preinstalled under the /etc/cron.daily directory.
 # As a system admin, monitor rkhunter.log file and if any suspicious file change(s) are found, raise 
 #alerts
+
+# If you wish, you can set up cron job to run on 4:15 am
+#crontab -l | { cat; echo '15 04 * * * /usr/bin/rkhunter --cronjob --update --quiet'; } | crontab -
 
 ###
 # Ref: 
