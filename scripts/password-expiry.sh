0d7f5f96c097c1bf86c05ab22ad2fb24b56a1210
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2023-05-18 16:30:07 +0530

    Create password-expiry.sh

---
+++ scripts/password-expiry.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SUB="nologin"
+pass_expiry_days=1000
+
+for USER_Entry in $(grep home /etc/passwd | cut -d':' -f 1,7)
+do
+  if [[ "$USER_Entry" == *"$SUB"* ]]; then
+    echo ""
+  else
+    name=$(echo $USER_Entry|cut -d ":" -f1)
+    # echo $name
+    if [ "$(chage -l $name | grep 'Password expires' | cut -d':' -f2)" == ' never' ]; then
+    # echo $name
+    # change password expiry to approx. 
+      chage -E $pass_expiry_days $name
+    fi
+
+  fi
+done
